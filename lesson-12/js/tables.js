e15bda10f3bd550d707ec510c1f3bde521e82e98
Scott McCrindle <iainmccrindle@gmail.com>
2025-04-04 11:19:28 -0400

    Compled all CSS and JS for a functional table

--- lesson-12/js/tables.js
+++ lesson-12/js/tables.js
@@ -1,1 +1,5 @@
 // STEP 11: Call to tablesorter script here
+// Grab the table
+const consolesTable = document.querySelector("table");
+// Enable TableSort.js on the table
+let myTable = new Tablesort(consolesTable);
